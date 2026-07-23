14547a8174232240abcf735d44d988ab264b1449
Luciano Rossi <lukio@gcoop.coop>
2026-07-23 08:23:09 -0300

    Prepare release 8.0

--- __init__.py
+++ __init__.py
@@ -2,19 +2,3 @@
 # This file is part of the sale_invoice_line_standalone module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.pool import Pool
-from . import party
-from . import sale
-from . import invoice
-
-
-def register():
-    Pool.register(
-        sale.Sale,
-        sale.SaleIgnoredInvoiceLine,
-        invoice.InvoiceLine,
-        party.Party,
-        module='sale_invoice_line_standalone', type_='model')
-    Pool.register(
-        sale.HandleInvoiceException,
-        module='sale_invoice_line_standalone', type_='wizard')
